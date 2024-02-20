16017b0669cd01af7b54b327a3ce03779b484d87
AlshimaaAlaa <155387177+AlshimaaAlaa@users.noreply.github.com>
2024-02-20 23:58:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Questions from './Questions/questions';
+
+function App() {
+  return (
+    <> 
+      <div>
+        <Questions/>
+      </div>
+    </>
+  );
+}
+
+export default App;
